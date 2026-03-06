1247e041b9037f7643488a261bdd0268067864f0
sanil011 <sanilp191@gmail.com>
2026-03-06 23:32:46 +0530

    feat:initialize, add webpack,babel and start running in local

---
+++ webpack/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                // this tell webpack to babel-loader for .js .ts .tsx .jsx file excluding noode_modules 
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    // bundle code should in build folder
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    // inject bundle.js file into index.html and place that html file into build folder
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+    ],
+    stats: 'errors-only',
+}
