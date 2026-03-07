8f1ba9f81d5b46bfd89aa6b212f24784b53c8441
sanil011 <sanilp191@gmail.com>
2026-03-08 00:21:12 +0530

    feat: add css loader and image loader to compiler image  and their import properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+import './style.css';
+import Image from "./laptop.jpg";
 export const App = () => {
-    return <h1>Hii Sanil</h1>
+    return <>
+        <h1>Hii Sanil</h1>
+        <img src={Image} alt="image" width='300' height='400' />
+    </>
 }
