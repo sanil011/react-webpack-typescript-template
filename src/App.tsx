c4a8a661562155e19f71fc3c0f5f8d17707de3f5
sanil011 <sanilp191@gmail.com>
2026-03-08 14:09:01 +0530

    feat: add react refresh (hot reloading)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './style.css';
 import Image from "./laptop.jpg";
+import ClickCounter from './ClickCounter';
 export const App = () => {
     return <>
         <h1>Hii Sanil</h1>
         <h1>React Webpack Typescript Template - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={Image} alt="image" width='300' height='400' />
+        <ClickCounter/>
     </>
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,14 +1,19 @@
 //  Contain configuration specific to dev
-
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const webpack = require('webpack')
 const { plugins } = require('./webpack.common')
 module.exports = {
     mode: "development",
+    devServer: {
+        hot: true,
+        open:true,
+    },
     devtool: 'cheap-module-source-map',
     plugins: [
         new webpack.DefinePlugin({
             'process.env.name': JSON.stringify('Sanil')
-        })
+        }),
+        new ReactRefreshWebpackPlugin(),
     ]
 }
 
